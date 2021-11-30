326d53c09f46359bd96a9ea62627836f0af558a9
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-11-30 13:15:52 -0500

    [7B] v0.3

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,4 @@
-# My fIRST PyGame, Tyler Fann, 11/30/21, 12:24PM, v0.2
+# My First PyGame, Tyler Fann, 11/30/21, 1:14PM, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -8,3 +8,12 @@ pygame.init()
 
 # Setup the Window to draw on.
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('My First PyGame') 
+
+# Setup color values.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PUKEBROWN = (150, 150, 25)
