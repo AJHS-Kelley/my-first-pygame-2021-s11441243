114e34a8be2d734cc50c3203cb541227e26480c1
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-11-30 12:25:17 -0500

    [7B] v0.2

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,10 @@
-# My fIRST PyGame, Tyler Fann, 11/30/21, 12:13PM, v0.0
+# My fIRST PyGame, Tyler Fann, 11/30/21, 12:24PM, v0.2
 
 import pygame, sys
 from pygame.locals import *
+
+# initialize PyGame
+pygame.init()
+
+# Setup the Window to draw on.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
