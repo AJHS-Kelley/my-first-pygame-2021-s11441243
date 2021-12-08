469cd874a11dd0adb892eccfc61678ab85cd12d4
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-12-08 11:48:32 -0500

    [7B] v0.1

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with PyGame, Tyler Fann, 12/08/21, 11:35, v0.0
+# Simple Animation with PyGame, Tyler Fann, 12/08/21, 11:47, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+# Setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
