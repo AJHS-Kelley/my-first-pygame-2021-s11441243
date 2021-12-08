f021a6726eca8e6052f224b033ad5d07043cf39b
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-12-08 11:52:07 -0500

    [7B] v0.2

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Tyler Fann, 12/08/21, 11:47, v0.1
+# Simple Animation with PyGame, Tyler Fann, 12/08/21, 11:51, v0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED= 4
