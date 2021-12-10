99d8e4105b2537df85d20ebbdeffa7c8f02ebea4
Tyler F <90405742+s11441243@users.noreply.github.com>
2021-12-10 11:40:59 -0500

    [7B] v1.0

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Tyler Fann, 11/30/21, 1:36PM, v0.5
+# My First PyGame, Tyler Fann, 12/10/21, 11:39AM, v1.0
 
 import pygame, sys
 from pygame.locals import *
@@ -30,3 +30,39 @@ textRect.centery = windowSurface.get_rect().centery
 
 #Fill in window background color.
 windowSurface.fill(PUKEBROWN)
+
+# Draw a polygon onto the screen.
+pygame.draw.polygon(windowSurface, GREEN, ((146,0), (291,106), (236,277), (56,277), (0,106)))
+
+# Draw lines on the screen.
+pygame.draw.line(windowSurface, RED, (60,60), (120,60), 4)
+pygame.draw.line(windowSurface, WHITE, (75,60), (60,75), 2)
+pygame.draw.line(windowSurface, BLUE, (0,150), (150,0),1)
+
+# Draw a circle.
+pygame.draw.circle(windowSurface, BLACK, (300, 50), 20, 0)
+
+# Draw an ellipse.
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+
+# Draw the test rectangle.
+pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+
+# Creat Pixel Array
+pixArray = pygame.PixelArray(windowSurface)
+pixArray[480][380] = BLUE
+del pixArray
+
+# Draw the text onto the surface.
+windowSurface.blit(text, textRect)
+
+# Update Pygame Display
+pygame.display.update()
+
+# Run hame loop
+while True:
+    for event in pygame.event.get():
+        if event.type == quit:
+            pygame.quit()
+            sys.exit()
+
