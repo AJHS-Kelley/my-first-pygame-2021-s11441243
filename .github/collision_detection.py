cc2093c41a4631b977dcdc0ebe38ebecf19b6f2f
Tyler F <90405742+s11441243@users.noreply.github.com>
2022-01-10 12:05:45 -0500

    7B v0.4

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Tyler Fann, January 10, 2022, 11:57am, v0.3
+# PyGame Collision Detection Practice, Tyler Fann, January 10, 2022, 12:05Pm, v0.4
 
 import pygame, sys, random
 from pygame.locals import *
@@ -17,3 +17,13 @@ pygame.display.set_caption('Collision Detection 2022')
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
+
+# Setup the player and food data structures. 
+foodCounter = 0
+NEWFOOD = 40 
+FOODSIZE = 20
+player = pygame.rect(300, 100, 50, 50)
+foods = []
+
+for i in range(20):
+    foods.append(pygame.rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
