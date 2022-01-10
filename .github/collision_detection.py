35938a0f76c6e8b009ebfd39bba7cb71d90d34b0
Tyler F <90405742+s11441243@users.noreply.github.com>
2022-01-10 11:40:03 -0500

    7B v0.1

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,1 +1,8 @@
-# PyGame Collision Detection Practice, Tyler Fann, January 10, 2022, 11:32am, v0.0
+# PyGame Collision Detection Practice, Tyler Fann, January 10, 2022, 11:39am, v0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
